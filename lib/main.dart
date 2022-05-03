7d2ae39062787155974592d78164b330bae47677
Raphael <raphaeldesouza@outlook.com>
2022-05-02 22:24:36 -0300

    feat: add new vm's

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tellstones_recreate/data/view_models/configuration_view_model.dart';
 import 'package:tellstones_recreate/data/view_models/stone_line_view_model.dart';
 import 'package:tellstones_recreate/data/view_models/stone_pool_view_model.dart';
+import 'package:tellstones_recreate/data/view_models/table_points_view_model.dart';
 import 'package:tellstones_recreate/presentation/home/home_page.dart';
 
 void main() {
@@ -11,7 +13,9 @@ void main() {
       //   create: (context) => getHomeController(),
       // ),
       ChangeNotifierProvider(create: (context) => StonePoolViewModel()),
-      ChangeNotifierProvider(create: (context) => StoneLineViewModel())
+      ChangeNotifierProvider(create: (context) => StoneLineViewModel()),
+      ChangeNotifierProvider(create: (context) => TablePointsViewModel()),
+      ChangeNotifierProvider(create: (context) => ConfigurationViewModel()),
     ],
     child: MaterialApp(
       initialRoute: '/',
--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,85 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tellstones_recreate/data/models/stones_enum.dart';
+import 'package:tellstones_recreate/data/view_models/configuration_view_model.dart';
 import 'package:tellstones_recreate/data/view_models/stone_line_view_model.dart';
 import 'package:tellstones_recreate/data/view_models/stone_pool_view_model.dart';
+import 'package:tellstones_recreate/presentation/home/widgets/empty_stone.dart';
 import 'package:tellstones_recreate/presentation/home/widgets/place_stone.dart';
 import 'package:tellstones_recreate/presentation/home/widgets/stone_pool_widget.dart';
 import 'package:tellstones_recreate/presentation/home/widgets/stone_widgert.dart';
 import 'package:tellstones_recreate/presentation/home/widgets/the_line_widget.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isDragging = false;
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Consumer<StonePoolViewModel>(
-              builder: ((context, controller, child) => StonePool(
-                  stones: controller.stonePool,
-                  onDragStarted: () {
-                    setState(() {
-                      isDragging = true;
-                    });
-                  },
-                  onDragEnd: () {
-                    setState(() {
-                      isDragging = false;
-                    });
-                  }))),
-          Consumer<StoneLineViewModel>(builder: (context, controller, child) {
-            return TheLine(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (controller.stoneLine.isEmpty && !isDragging)
-                    const Stone.none(),
-                  // DraggableStone(
-                  //     child: Stone(
-                  //   stone: Stones.balance,
-                  // )),
-                  if (isDragging)
-                    DragTarget<Stones>(
-                      builder: (context, candidateData, rejectedData) =>
-                          const PlaceStone(),
-                      onAccept: (stoneType) {
-                        Provider.of<StoneLineViewModel>(context, listen: false)
-                            .addStoneToLeft(stoneType);
-                        Provider.of<StonePoolViewModel>(context, listen: false)
-                            .removeStone(stoneType);
-                      },
-                    ),
-                  ...controller.stoneLine
-                      .map((stone) => Stone(
-                            stone: stone.type,
-                          ))
-                      .toList(),
-                  if (isDragging && controller.stoneLine.isNotEmpty)
-                    DragTarget<Stones>(
-                      builder: (context, candidateData, rejectedData) =>
-                          const PlaceStone(),
-                      onAccept: (stoneType) {
-                        controller.addStoneToRight(stoneType);
-                        Provider.of<StonePoolViewModel>(context, listen: false)
-                            .removeStone(stoneType);
-                      },
-                    )
-                ],
-              ),
-            );
-          }),
-        ],
-      )),
-    );
+    return Consumer<ConfigurationViewModel>(
+        builder: (context, configurationViewModel, child) {
+      return Scaffold(
+        body: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Consumer<StonePoolViewModel>(
+                builder: ((context, stonePoolViewModel, child) => StonePool(
+                    stones: stonePoolViewModel.stonePool,
+                    onDragStarted: () =>
+                        configurationViewModel.setIsDragging(true),
+                    onDragEnd: () =>
+                        configurationViewModel.setIsDragging(false)))),
+            Consumer<StoneLineViewModel>(
+                builder: (context, stoneLineViewModel, child) {
+              return TheLine(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    if (stoneLineViewModel.stoneLine.isEmpty &&
+                        !configurationViewModel.isDragging)
+                      const EmptyStone(),
+                    // DraggableStone(
+                    //     child: Stone(
+                    //   stone: Stones.balance,
+                    // )),
+                    if (configurationViewModel.isDragging)
+                      DragTarget<Stones>(
+                        builder: (context, candidateData, rejectedData) =>
+                            const PlaceStone(),
+                        onAccept: (stoneType) {
+                          Provider.of<StoneLineViewModel>(context,
+                                  listen: false)
+                              .addStoneToLeft(stoneType);
+                          Provider.of<StonePoolViewModel>(context,
+                                  listen: false)
+                              .removeStone(stoneType);
+                        },
+                      ),
+                    ...stoneLineViewModel.stoneLine
+                        .map((stone) => Stone(
+                              stone: stone.type,
+                            ))
+                        .toList(),
+                    if (configurationViewModel.isDragging &&
+                        stoneLineViewModel.stoneLine.isNotEmpty)
+                      DragTarget<Stones>(
+                        builder: (context, candidateData, rejectedData) =>
+                            const PlaceStone(),
+                        onAccept: (stoneType) {
+                          stoneLineViewModel.addStoneToRight(stoneType);
+                          Provider.of<StonePoolViewModel>(context,
+                                  listen: false)
+                              .removeStone(stoneType);
+                        },
+                      )
+                  ],
+                ),
+              );
+            }),
+          ],
+        )),
+      );
+    });
   }
 }
